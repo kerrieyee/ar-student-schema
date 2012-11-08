e97171ac747ec6ccd3eacec9601b68fe44acd480
kerrieyee <kerrie@kerrieyee.com>
2012-11-07 18:44:37 -0800

    first orm model, student class

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,20 @@
 require_relative '../../db/config'
 
 # implement your Student model here
+ # validates_inclusion_of :email, :in => %w( @ . ), :message => "Please make sure you have typed in a valid email address."
+ # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :save
+
+
+
+class Student < ActiveRecord::Base
+
+	def name
+		first_name + " " + last_name
+	end
+
+	def age
+		now = Date.today
+		now.year - birthday.year
+	end
+
+end 
