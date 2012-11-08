f54aef473db949154ecf2109c8b4bb26e6f4207e
kerrieyee <kerrie@kerrieyee.com>
2012-11-07 20:10:11 -0800

    did validations for student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,12 +1,14 @@
 require_relative '../../db/config'
 
 # implement your Student model here
- # validates_inclusion_of :email, :in => %w( @ . ), :message => "Please make sure you have typed in a valid email address."
- # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :save
-
+ 
 
 
 class Student < ActiveRecord::Base
+ 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+ 	validates_exclusion_of :age, :in => 0..4, :message => "sorry you are too young for school"
+ 	validates_uniqueness_of :email
+ 	validates_format_of :phone, :with => /\(\d{3}\)\s\d{3}\-\d{4}/
 
 	def name
 		first_name + " " + last_name
